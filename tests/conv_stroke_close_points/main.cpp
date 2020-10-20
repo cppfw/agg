189f7d97530a375b2007a255ad62c90ef346e2fe
Ivan Gagis <igagis@gmail.com>
2020-10-20 17:57:42 +0300

    linux build fix

--- tests/conv_stroke_close_points/main.cpp
+++ tests/conv_stroke_close_points/main.cpp
@@ -1,6 +1,6 @@
+#include <iostream>
 #include <iomanip>
-
-#include <utki/debug.hpp>
+#include <cassert>
 
 #include "../../src/agg/include/agg_path_storage.h"
 #include "../../src/agg/include/agg_conv_stroke.h"
@@ -14,11 +14,11 @@ int main(int argc, const char** argv){
 	path.line_to(169.99999868865830877, 20.000000000000014211);
 
 	path.rewind(0);
-	TRACE_ALWAYS(<< "print path:" << std::endl)
+	std::cout << "print path:" << std::endl;
 	{
 		double x, y;
 		for(auto cmd = path.vertex(&x, &y); cmd != agg::path_cmd_stop; cmd = path.vertex(&x, &y)){
-			TRACE_ALWAYS(<< std::setprecision(20) << "cmd = " << cmd << ", xy = (" << x << ", " << y << ")" << std::endl)
+			std::cout << std::setprecision(20) << "cmd = " << cmd << ", xy = (" << x << ", " << y << ")" << std::endl;
 		}
 	}
 
@@ -29,12 +29,12 @@ int main(int argc, const char** argv){
 
 	stroke.rewind(0);
 
-	TRACE_ALWAYS(<< "print stroke:" << std::endl)
+	std::cout << "print stroke:" << std::endl;
 	{
 		double x, y;
 		for(auto cmd = stroke.vertex(&x, &y); cmd != agg::path_cmd_stop; cmd = stroke.vertex(&x, &y)){
-			TRACE_ALWAYS(<< std::setprecision(20) << "cmd = " << cmd << ", xy = (" << x << ", " << y << ")" << std::endl)
-			ASSERT_ALWAYS(x <= 175)
+			std::cout << std::setprecision(20) << "cmd = " << cmd << ", xy = (" << x << ", " << y << ")" << std::endl;
+			assert(x <= 175);
 		}
 	}
 

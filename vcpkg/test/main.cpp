2f65c109e182f76e6c63f8db60ee99e2203eb70c
Ivan Gagis <igagis@gmail.com>
2024-11-26 12:44:05 +0200

    vcpkgize

---
+++ vcpkg/test/main.cpp
@@ -0,0 +1,13 @@
+#include <agg/agg_trans_affine.hpp>
+
+using namespace std::string_view_literals;
+
+int main(int argc, const char** argv){
+    agg::trans_affine t(10, 20, 30, 40, 50, 60);
+
+    t.reset();
+
+    std::cout << "t = " << t.sx << ", " << t.sy << ", " << t.tx << ", " << t.ty << ", " << t.shx << ", " << t.shy << std::endl;
+
+    return 0;
+}
